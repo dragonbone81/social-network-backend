05034896d3d5f1eb623237df3b8ab2a23292f06c
Christian <cvernikoff@gmail.com>
2018-10-16 11:07:12 -0700

    start

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'me',
+//     password : 'secret',
+//     database : 'my_db'
+// });
+const app = express();
+const port = 3000;
+
+
+app.use(morgan('short'));
+
+app.get('/', (req, res) => {
+    res.send("HELLO");
+});
+
+app.listen(port, () => console.log("Server Started!"));
