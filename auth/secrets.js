74a5bb8ba5e987a4dfb5534ae5f78700e6416436
Christian <cvernikoff@gmail.com>
2018-10-17 22:25:01 -0700

    bcrypt

--- auth/secrets.js
+++ auth/secrets.js
@@ -1,8 +1,8 @@
 module.exports = {
-    user: process.env.DATABASE_USER || 'epakzsujlinauk',
-    host: process.env.DATABASE_HOST || 'ec2-107-20-211-10.compute-1.amazonaws.com',
-    database: process.env.DATABASE_NAME || 'd7gfv8danvaan6',
-    password: process.env.DATABASE_PASSWORD || 'a0cd8282b8bed1b1dcfe15a2257af9f5dc11382d3b1b657f5d5a69a3e43065ec',
-    port: process.env.DATABASE_PORT || 5432,
-    secretKey: process.env.SECRET_KEY || 'v2oesj(lzzwe_&-^+u3(s$&d-76*!!orb&z^xdz&%#_t@bry%0'
+    user: process.env.DATABASE_USER || '',
+    host: process.env.DATABASE_HOST || '',
+    database: process.env.DATABASE_NAME || '',
+    password: process.env.DATABASE_PASSWORD || '',
+    port: process.env.DATABASE_PORT || 3000,
+    secretKey: process.env.SECRET_KEY || ''
 };
